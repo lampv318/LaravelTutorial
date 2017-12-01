bb31b1a92e5c423e3d6a370c3a3a854e1a9a744f
lampv <lampvhy@gmail.com>
2017-12-01 14:08:00 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,48 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hello',function(){
+  return "hello world";
+});
+
+Route::get('hello1',function(){
+  echo "hello 1";
+});
+
+// truyen tham so
+
+Route::get('hoten/{ten}',function($ten){
+  echo "ten cua ban ".$ten;
+});
+
+Route::get('ngay/{ngay}',function($ngay){
+  echo "Ngay : ".$ngay;
+})->  where(['ngay'=> '[0-9]+']);
+
+// dinh danh
+
+Route::get('route',['as'=>'myroute',function(){
+  echo "my route";
+}]);
+
+Route::get('route2',function(){
+  echo "this is route 2";
+})->name('route2');
+
+Route::get('goiten',function(){
+  return redirect()->route('route2');
+});
+
+//group
+Route::group(['prefix'=>'mygroup'], function(){
+  Route::get('user1',function(){
+    echo "this is group 1";
+  });
+  Route::get('user2',function(){
+    echo "this is group 2";
+  });
+  Route::get('user3',function(){
+    echo "this is group 3";
+  });
+});
